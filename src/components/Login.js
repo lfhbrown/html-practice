fcef9d110238b68b7e666e6e3f45e81320e2b713
6Weapons <89061083+lfhbrown@users.noreply.github.com>
2023-10-08 23:43:11 -0700

    Pablo you the man

--- src/components/Login.js
+++ src/components/Login.js
@@ -30,7 +30,7 @@ const Login = () => {
               value={"Email"}
             />
             <input
-              type="text"
+              type="password"
               id="password"
               name="password"
               required
